1d6cc452a3edce453ff4b606c941c5ef44d59e0a
allain <allain.lalonde@gmail.com>
2015-02-28 20:19:44 -0500

    starting interface

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = function () {
-  return 'Hello, world';
+var Readable = require('stream').Readable;
+
+module.exports = {
+  comments: fetchComments
 };
+
+function fetchComments(videoId) {
+  var readable = new Readable();
+
+  readable._read = function() {
+
+  };
+
+  return readable;
+}
--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
-var expect = require('chai').expect,
-    youtubeExtractors = require('..');
+var assert = require('chai').assert;
+var extractors = require('..');
+var stream = require('stream');
 
 describe('youtube-extractors', function() {
-  it('should say hello', function(done) {
-    expect(youtubeExtractors()).to.equal('Hello, world');
-    done();
+  it('should implements comments extractor', function() {
+    assert.isFunction(extractors.comments);
+    var result = extractors.comments('Gaid72fqzNE');
+    assert(result instanceof stream.Readable);
   });
 });
